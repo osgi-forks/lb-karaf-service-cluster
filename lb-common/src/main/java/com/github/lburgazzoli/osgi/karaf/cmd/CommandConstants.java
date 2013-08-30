ca1583d812208ee40825a178e4c19d7e450805a4
lburgazzoli <lburgazzoli@gmail.com>
2013-08-30 13:11:33 +0200

    cleanup

--- lb-common/src/main/java/com/github/lburgazzoli/osgi/karaf/cmd/CommandConstants.java
+++
@@ -1,9 +0,0 @@
-package com.github.lburgazzoli.osgi.karaf.cmd;
-
-/**
- *
- */
-public class CommandConstants {
-    public static final String FLAG_YES = "Y";
-    public static final String FLAG_NO  = "N";
-}
